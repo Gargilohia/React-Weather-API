0ab65566d639afee9714c37cf3fefeb1f6f4d77e
Gargi Lohia <53905460+Gargilohia@users.noreply.github.com>
2023-01-12 02:16:25 +0530

    second commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApioptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '5b8bacb8b7mshe9de226767b4cf2p1139cdjsn7e4fc640337b',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+
+export const WEATHER_API_KEY = "6d7b0a3dd2b56405690139dd4f1735ff"
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
-
+import { GEO_API_URL, geoApioptions } from "../../api";
 const Search = ({onSearchChange}) => {
 
     const [search, setSearch] = useState(null);
@@ -12,12 +12,29 @@ const Search = ({onSearchChange}) => {
 
     }
 
+    const loadOptions = (inputValue) => {
+        return fetch(`${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, geoApioptions)
+        .then(response => response.json())
+        .then((response) => {
+            return {
+                options: response.data.map((city)=>{
+                    return{
+                    value: `${city.latitude} ${city.longitude}`,
+                    label: `${city.name}, ${city.countryCode}`,
+                };
+            }),
+            };
+        })
+        .catch(err => console.error(err));
+    };
+
     return(
         < AsyncPaginate
           placeholder="Search for city" 
           debounceTimeout={600}
           value = {search}
           onChange = {handleonChange}
+          loadOptions = {loadOptions}
           />
     )
 }
